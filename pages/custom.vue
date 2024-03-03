2dc2aee2a493c36790367738362a64c0fbfb1d65
MsWoo <minsoo010@naver.com>
2024-03-03 16:27:12 +0900

    study: server/api, server/routes 학습

--- pages/custom.vue
+++ pages/custom.vue
@@ -2,7 +2,12 @@
 definePageMeta({
   layout: "custom"
 })
+
+const { data } = await useFetch('/api/hello')
+console.log(data)
+
 </script>
 <template>
   <h1>Custom Page</h1>
+  <h1>{{ data }}</h1>
 </template>
